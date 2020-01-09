37e1a16c1e6ca284b937372e6e9f746d3050afda
Florian AUBERVAL <mecaman01@gmail.com>
2020-01-09 23:09:51 +0100

    initial commit

---
+++ Button.py
@@ -0,0 +1,56 @@
+class Widget :
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.rect = [x, y, x+w, y+h]
+
+    def detect(self, x, y):
+        return (self.x < x < self.x + self.w) and (self.y < y < self.y + self.h)
+
+
+
+class Button(Widget) :
+    """Boutton de sous-widget canvas"""
+    def __init__(self, canvas, x, y, w, h, f, colorSchem=["#000000", "#444433", "#998755"], name="Button"):
+        Widget.__init__(self, x, y, w, h)
+        self.cv = canvas
+        self.name = name
+        self.function = f
+        self.thm = colorSchem
+
+        self.wdg = self.cv.create_rectangle( self.rect, fill=self.thm[0], outline="")
+
+        self.selected = False
+        self.pushed = False
+
+
+    def onMotion(self, cursor):
+        self.selected = self.detect(cursor["x"], cursor["y"])
+        if not self.pushed:
+            if self.selected :
+                self.render(self.thm[1])
+            else:
+                self.render(self.thm[0])
+
+    def onPress(self, cursor):
+        if self.selected :
+            self.pushed = True
+            self.render(self.thm[2])
+
+    def onRelease(self, cursor):
+        if self.selected:
+            if self.pushed:
+                self.function()
+                self.render(self.thm[1])
+            else:
+                self.render(self.thm[0])
+        else:
+            self.render(self.thm[0])
+        self.pushed = False
+
+
+    def render(self, color):
+        self.cv.delete(self.wdg)
+        self.wdg = self.cv.create_rectangle( self.rect, fill=color, outline="")
